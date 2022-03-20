24c4c74c8a4cba6f51b2ef393db796f6d3f663ac
Mihir Manek <mihir.manek@somaiya.edu>
2022-03-20 19:40:24 -0400

    completed labs

---
+++ src/components/Labs/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Classes from "./Classes";
+import ConditionalOutput from "./ConditionalOutput";
+import Styles from "./Styles";
+import TodoList from "./Todo";
+
+const Labs = () => {
+  return (
+    <>
+      <h1>Labs</h1>
+      <Link to="/hello">Hello</Link> |<Link to="/tuiter">Tuiter</Link>
+      <ConditionalOutput />
+      <Styles />
+      <Classes />
+      <TodoList />
+    </>
+  );
+};
+
+export default Labs;
