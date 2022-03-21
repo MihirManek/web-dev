94a20dc5b97c08732325f7363c0ee91b73b00c3d
Mihir Manek <mihir.manek@somaiya.edu>
2022-03-20 22:44:47 -0400

    added basic tuiter components

--- src/components/Tuiter/index.js
+++ src/components/Tuiter/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ExploreScreen from "./explore-screen";
+import ExploreComponent from "./explore-screen/explore-component";
+import HomeScreen from "./home-screen";
+import NavigationSidebar from "./navigation-sidebar";
+import PostList from "./post-summary-list";
+import PostSummaryItem from "./post-summary-list/post-summary-item";
+import WhoToFollowList from "./who-to-follow-list";
+import WhoToFollowListItem from "./who-to-follow-list/who-to-follow-list-item";
 
 const Tuiter = () => {
   return (
     <>
-      <h1>Tuiter</h1>
-      <Link to="/hello">Hello</Link> |<Link to="/labs">Labs</Link>
+      <HomeScreen />
     </>
   );
 };
